8f55cb41a51d551fd1bee66415938b370de692ca
Harsh Helaiya <Harsh.Helaiya@teknopoint.local>
2023-03-28 18:58:43 +0530

    commit

--- node.js/server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const app = require('./app');
-const server = http.createServer(app);
-
-// api
-server.listen(3000, () => {
-  console.log("Server started on port 8000");
-});
